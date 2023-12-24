d444d607073c004a7669abfc42361f1b0c28e132
user <user@LILIT>
2023-12-24 14:08:05 +0200

    Добавьте файлы проекта.

---
+++ Slovomorf/Slovomorf.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void generateFunnyWords(const string* words, int size)
+{
+    cout << "Смешные слова:\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        if (words[i].length() >= 3)
+        {
+            string suffix = words[i].substr(words[i].length() - 3);
+
+            for (int j = 0; j < size; j++)
+            {
+                if (j != i && words[j].length() >= 3 && words[j].substr(0, 3) == suffix)
+                {
+                    cout << words[i] << words[j].substr(3) << "\n";
+                   
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    const string filename = "D:\\russian.txt";
+    ifstream inFile(filename);
+
+    if (!inFile.is_open())
+    {
+        cerr << "Не получилось открыть словарь.\n";
+        return 1;
+    }
+
+    string* words = new string[10000];
+    int size = 0;
+    string word;
+
+    while (inFile >> word)
+    {
+        words[size++] = word;
+    }
+
+    inFile.close();
+
+    generateFunnyWords(words, size);
+
+    delete[] words;
+}
